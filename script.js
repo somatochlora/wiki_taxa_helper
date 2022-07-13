b15782c8df44592468b049af20d83485fe453c14
Reuven Martin <jaeger33@gmail.com>
2022-07-13 13:37:52 -0400

    move javascript to new file

---
+++ script.js
@@ -0,0 +1,136 @@
+"use strict";
+
+const getJSON = async url => {
+    const response = await fetch(url);
+    if(!response.ok) // check if response worked (no 404 errors etc...)
+        throw new Error(response.statusText);
+
+    const data = response.json(); // get JSON from the response
+    return data; // returns a promise, which resolves to this data value
+}
+
+class Taxon {
+    constructor(taxonData) {
+        this.id = taxonData.id;
+        this.latinName = taxonData.name;
+        this.rank = taxonData.rank;
+        this.children = taxonData.children;
+        this.hasChildren = (this.children != undefined);
+        if (this.hasChildren) this.childrenNum = this.children.length;
+        this.commonName = taxonData.preferred_common_name;
+        this.hasCommonName = (this.commonName != undefined);
+    }
+
+    addObservations(observationData) {
+        this.observations = [];
+        for (let observation of observationData) {
+            let newObs = {};
+            newObs.qualityGrade = observation.quality_grade;
+            newObs.datetime = observation.time_observed_at;
+            newObs.id = observation.id;
+            newObs.geoprivacy = observation.geoprivacy;
+            if (newObs.geoprivacy = null) newObs.geoprivacy = "open";
+            newObs.photos = observation.photos;
+            newObs.photoNum = newObs.photos.length;
+            newObs.location = observation.location;
+
+            this.observations.push(newObs);
+        };
+    }
+
+    async getLicensedPhotos () {
+        const response = await fetch("https://api.inaturalist.org/v1/observations?photo_license=cc-by%2Ccc-by-sa%2Ccc0&taxon_id=" + this.id)
+        if(!response.ok)
+            throw new Error(response.statusText);
+        const data = response.json();
+        return data;
+    }
+
+    getWikidataID () {
+        pass;
+    }
+}
+
+/*async function getWikiDataTaxon (taxonID) {
+    const sparqlQuery = 'SELECT ?item\nWHERE\n{\n?item wdt:P3151 "' + taxonID + '".\n}';
+    const response = await fetch('https://query.wikidata.org/sparql?query=' + encodeURIComponent(sparqlQuery))
+    if(!response.ok)
+        throw new Error(response.statusText);
+    const data = response.text();
+    return data;
+}*/
+
+
+
+document.querySelector('#taxonSubmit').addEventListener('click', function() {
+    let taxonID = document.querySelector('#iNatTaxonID').value;
+    getJSON("https://api.inaturalist.org/v1/taxa/" + taxonID).then(data => {
+        let resultsDiv = document.querySelector('#results');
+        let taxa = [];
+        let parentTaxon = new Taxon (data.results[0]);
+
+        // TODO: add pagination to get all children
+
+        for (let childIndex = 0; childIndex < parentTaxon.childrenNum; childIndex++) {
+            taxa.push(new Taxon (parentTaxon.children[childIndex]));
+            taxa[childIndex].getLicensedPhotos().then(childData => {
+                
+                let observationDiv = document.createElement('div');
+                let para = document.createElement('p');
+                let imagesDiv = document.createElement('div');
+
+                taxa[childIndex].addObservations(childData.results);
+
+                para.textContent = taxa[childIndex].commonName + ": " + childData.total_results + " observations with licensed photos";
+                observationDiv.appendChild(para);
+
+                if (childData.total_results != 0) {
+                    for (let observationIndex = 0; observationIndex < taxa[childIndex].observations.length; observationIndex++) {                                    
+                        for (let imageIndex = 0; imageIndex < taxa[childIndex].observations[observationIndex].photoNum; imageIndex++) {
+                            
+                            let link = document.createElement('a');
+                            link.target = "_blank";
+                            link.href = "https://www.inaturalist.org/observations/" + taxa[childIndex].observations[observationIndex].id;
+                            link.id = childIndex + "," + observationIndex + "," + imageIndex;
+
+                            let image = document.createElement('img');
+                            // need to check license on each photo individually todo
+                            image.src = taxa[childIndex].observations[observationIndex].photos[imageIndex].url;
+                            let borderColour;
+                            switch (taxa[childIndex].observations[observationIndex].qualityGrade) {
+                                case "research": 
+                                    borderColour = "green";
+                                    break;
+                                case "needs_id":
+                                    borderColour = "yellow";
+                                    break;
+                                case "casual":
+                                    borderColour = "red";
+                                    break;
+                            }
+                            image.style = "border:4px solid " + borderColour + ";";
+
+                            link.appendChild(image);
+                            link.style = "margin:4px;"
+
+                            imagesDiv.appendChild(link);
+                        }
+                        observationDiv.appendChild(imagesDiv);
+                    } 
+                }                           
+                resultsDiv.appendChild(observationDiv);
+
+                /*getWikiDataTaxon(data.results[0].children[i].id).then(wdData => {
+                    alert(wdData);
+                }).catch(error => alert("error here" + error));*/
+
+            }).catch(error => {
+                alert("children api call error:" + error)
+            });                  
+        }
+    }).catch(error => {
+        alert("parent api call error:" + error);
+    });
+});
+
+
