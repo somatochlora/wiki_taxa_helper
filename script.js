21b94868e3b00fa9c75e1fe2f08a39cf3cbee433
Reuven Martin <jaeger33@gmail.com>
2022-07-14 14:28:12 -0400

    extensive refactor

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 "use strict";
 
-// Number of cild taxa to get at one time
-const childSetNum = 10;
-
 // Number of photos to display at a time
-const photoDisplayNum = 20;
+const PHOTODISPLAYNUM = 20;
+
+let children = {};
+let parentTaxon;
+
+const iNatPhotosDiv = document.querySelector('#inat-photos');
+const prevChildButton = document.querySelector('#prev-child');
+const nextChildButton = document.querySelector('#next-child');
+const prevPhotosButton = document.querySelector('#prev-photos');
+const nextPhotosButton = document.querySelector('#next-photos');
 
 async function getJSON(url) {
     const response = await fetch(url);
@@ -13,6 +19,56 @@ async function getJSON(url) {
 
     const data = response.json(); // get JSON from the response
     return data; // returns a promise, which resolves to this data value
+}
+
+class Photo {
+    constructor(observationData, photoNum) {
+        this.id = observationData.photos[photoNum].id;
+        this.license = observationData.photos[photoNum].license_code;
+        this.url = observationData.photos[photoNum].url;
+        this.attribution = observationData.photos[photoNum].attribution;
+        this.observationId = observationData.id;
+        this.qualityGrade = observationData.quality_grade;
+    }
+
+    // does photo have a commons-compatible license?
+    isLicensed() {
+        switch (this.license) {
+            case "cc0": 
+            case "cc-by": 
+            case "cc-by-sa":
+                return true;
+        }
+        return false;
+    }
+
+    returnDiv() {
+        let image = document.createElement('img');
+        image.src = this.url;
+        let borderColour;
+        switch (this.qualityGrade) {
+            case "research": 
+                borderColour = "green";
+                break;
+            case "needs_id":
+                borderColour = "yellow";
+                break;
+            case "casual":
+                borderColour = "red";
+                break;
+        }
+        image.style = "border:4px solid " + borderColour + ";";
+
+        let link = document.createElement('a');
+        link.target = "_blank";
+        link.href = "https://www.inaturalist.org/observations/" + this.observationId;     
+        link.style = "margin:4px;"
+
+        link.appendChild(image);       
+
+        return link;
+    }
+
 }
 
 class Taxon {
@@ -25,118 +81,106 @@ class Taxon {
         if (this.hasChildren) this.childrenNum = this.children.length;
         this.commonName = taxonData.preferred_common_name;
         this.hasCommonName = (this.commonName != undefined);
+        this.observations = [];
+        this.photos = [];
     }
 
-    addObservations(observationData) {
-        this.observations = [];
-        for (let observation of observationData) {
+    addObservations(observationData) {        
+        
+        // this is NOT the same as the length of this.observations, as that only returns the number of observations in the current json request
+        this.observationCount = observationData.total_results;
+        if (this.observationCount == 0) return;
+        for (let observation of observationData.results) {
             let newObs = {};
             newObs.qualityGrade = observation.quality_grade;
             newObs.datetime = observation.time_observed_at;
             newObs.id = observation.id;
             newObs.geoprivacy = observation.geoprivacy;
-            if (newObs.geoprivacy = null) newObs.geoprivacy = "open";
-            newObs.photos = observation.photos;
-            newObs.photoNum = newObs.photos.length;
+            if (newObs.geoprivacy = null) newObs.geoprivacy = "open";            
+            for (let i = 0; i < observation.photos.length; i++) {
+                let curPhoto = new Photo(observation, i);
+                if (curPhoto.isLicensed) this.photos.push(curPhoto);
+            }
             newObs.location = observation.location;
 
             this.observations.push(newObs);
         };
     }
 
-    async getLicensedPhotos () {
-        const data = await getJSON("https://api.inaturalist.org/v1/observations?photo_license=cc-by%2Ccc-by-sa%2Ccc0&taxon_id=" + this.id)
+    async getLicensedPhotos (maxId = -1) {
+        let maxIdStr = "";
+        if (maxId != -1) {
+            maxIdStr = "&id_below=" + maxId;
+        }
+        let perPageStr = "&per_page=" + PHOTODISPLAYNUM;
+        const data = await getJSON("https://api.inaturalist.org/v1/observations?photo_license=cc-by%2Ccc-by-sa%2Ccc0&taxon_id=" + this.id + maxIdStr + perPageStr)
         return data;
     }
 
-    async getWikidataID () {
+    async getWikidataId () {
         const sparqlQuery = `SELECT ?item
 WHERE
 {
 ?item wdt:P3151 "` + this.id + `".
 }`;
         const data = await getJSON('https://query.wikidata.org/sparql?query=' + encodeURIComponent(sparqlQuery) + '&format=json');
-        this.wikidataID = data.results.bindings[0].item.value;
+        this.wikidataID = data?.results?.bindings[0]?.item?.value;
+    }
+    
+    async loadChild () {
+        let observationsData = await this.getLicensedPhotos()
+        this.addObservations(observationsData)
+        await this.getWikidataId();
+    }
+
+    async getMorePhotos (maxId) {
+        let observationsData = await this.getLicensedPhotos(maxId)
+        this.addObservations(observationsData)
+    }
+
+    makePhotos (start = 0) {
+        let photosDiv = document.createElement('div');
+        for (let i = start; i < start + PHOTODISPLAYNUM; i++) {
+            if (i == this.photos.length) break;
+            photosDiv.appendChild(this.photos[i].returnDiv());
+        }
+        return photosDiv;
     }
 }
 
 document.querySelector('#taxonSubmit').addEventListener('click', function() {
     let taxonID = document.querySelector('#iNatTaxonID').value;
     getJSON("https://api.inaturalist.org/v1/taxa/" + taxonID)
-    .then(data => {
-        let resultsDiv = document.querySelector('#results');
-        let taxa = {};
-        let parentTaxon = new Taxon (data.results[0]);
+    .then(data => {        
+        
+        parentTaxon = new Taxon (data.results[0]);
 
+        let curChild = new Taxon(parentTaxon.children[0]);
+        children[curChild.id] = curChild;
+        
+        curChild.loadChild()
+        .then(() => {
+            let para = document.createElement('p');
 
-        // TODO: add pagination to get all children
+            if (curChild.hasCommonName) para.textContent = curChild.commonName;
+            else para.textContent = curChild.latinName;
+            para.textContent += ": " + curChild.observationCount + " observations with licensed photos";
 
-        for (let childIndex = 0; childIndex < parentTaxon.childrenNum; childIndex++) {
-            let curChildID = parentTaxon.children[childIndex].id;
-            taxa[curChildID] = new Taxon (parentTaxon.children[childIndex]);
-            taxa[curChildID].getLicensedPhotos()
-            .then(childData => {
-                taxa[curChildID].getWikidataID()
-                .then()
-                .catch(error => alert("Wikidata API call error: " + error));
-                let observationDiv = document.createElement('div');
-                let para = document.createElement('p');
-                let imagesDiv = document.createElement('div');
+            iNatPhotosDiv.appendChild(para);
+            iNatPhotosDiv.appendChild(curChild.makePhotos());
 
-                taxa[curChildID].addObservations(childData.results);
-                if (taxa[curChildID].hasCommonName) para.textContent = taxa[curChildID].commonName;
-                else para.textContent = taxa[curChildID].latinName;
-                para.textContent += ": " + childData.total_results + " observations with licensed photos";
-                observationDiv.appendChild(para);
+            prevChildButton.disabled = false;
+            nextChildButton.disabled = false;
+            prevPhotosButton.disabled = false;
+            nextPhotosButton.disabled = false;
 
-                if (childData.total_results != 0) {
-                    for (let observationIndex = 0; observationIndex < taxa[curChildID].observations.length; observationIndex++) {                                    
-                        for (let imageIndex = 0; imageIndex < taxa[curChildID].observations[observationIndex].photoNum; imageIndex++) {
-                            
-                            let link = document.createElement('a');
-                            link.target = "_blank";
-                            link.href = "https://www.inaturalist.org/observations/" + taxa[curChildID].observations[observationIndex].id;
-                            link.id = childIndex + "," + observationIndex + "," + imageIndex;
-
-                            let image = document.createElement('img');
-                            // need to check license on each photo individually todo
-                            image.src = taxa[curChildID].observations[observationIndex].photos[imageIndex].url;
-                            let borderColour;
-                            switch (taxa[curChildID].observations[observationIndex].qualityGrade) {
-                                case "research": 
-                                    borderColour = "green";
-                                    break;
-                                case "needs_id":
-                                    borderColour = "yellow";
-                                    break;
-                                case "casual":
-                                    borderColour = "red";
-                                    break;
-                            }
-                            image.style = "border:4px solid " + borderColour + ";";
-
-                            link.appendChild(image);
-                            link.style = "margin:4px;"
-
-                            imagesDiv.appendChild(link);
-                        }
-                        observationDiv.appendChild(imagesDiv);
-                    } 
-                }                           
-                resultsDiv.appendChild(observationDiv);
-
-                /*getWikiDataTaxon(data.results[0].children[i].id).then(wdData => {
-                    alert(wdData);
-                }).catch(error => alert("error here" + error));*/
-
-            })
-            .catch(error => {
-                alert("children api call error:" + error)
-            });                  
-        }
+        })
+        .catch(error => {
+            alert("child api call error: " + error);
+        });
     })
     .catch(error => {
-        alert("parent api call error:" + error);
+        alert("parent api call error: " + error);
     });
 });
 
